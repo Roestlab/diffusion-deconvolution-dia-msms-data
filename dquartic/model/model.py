Add encoder for conditioning signal, fix loss calculation, and add optional ms1 loss component

dquartic/model/model.py
@@ -1,17 +1,20 @@
-import sys
-
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
-
-import wandb
 
 from .model_interface import ModelInterface
 from .building_blocks import get_beta_schedule, get_alpha, get_alpha_bar
 
 
 class DDIMDiffusionModel(ModelInterface):
-    def __init__(self, model_class, num_timesteps=1000, beta_start=0.001, beta_end=0.00125, device="cuda", **kwargs):
+    def __init__(
+        self,
+        model_class,
+        num_timesteps=1000,
+        beta_start=0.001,
+        beta_end=0.00125,
+        device="cuda",
+        **kwargs,
+    ):
         super().__init__()
         self.model = None
         self.build(model_class, **kwargs)
@@ -33,7 +36,10 @@ class DDIMDiffusionModel(ModelInterface):
         sqrt_alpha_bar_t = torch.sqrt(self.alpha_bar[t])[:, None, None]
         sqrt_one_minus_alpha_bar_t = torch.sqrt(1 - self.alpha_bar[t])[:, None, None]
 
-        return sqrt_alpha_bar_t * x_start + sqrt_one_minus_alpha_bar_t * noise
+        return (
+            sqrt_alpha_bar_t * x_start + sqrt_one_minus_alpha_bar_t * noise,
+            sqrt_one_minus_alpha_bar_t,
+        )
 
     def p_sample(self, x_t, x_cond, t, eta=0.0):
         """
@@ -46,7 +52,6 @@ class DDIMDiffusionModel(ModelInterface):
         eps_pred = self.model(x_t, x_cond, t_tensor)
 
         # Compute x_{t-1}
-        alpha_t = self.alpha[t]
         alpha_bar_t = self.alpha_bar[t]
         sqrt_alpha_bar_t = torch.sqrt(alpha_bar_t)
         sqrt_one_minus_alpha_bar_t = torch.sqrt(1 - alpha_bar_t)
@@ -87,7 +92,7 @@ class DDIMDiffusionModel(ModelInterface):
 
         return x_t
 
-    def train_step(self, x_start, x_cond, noise=None):
+    def train_step(self, x_start, x_cond, noise=None, ms1_loss_weight=0.0):
         """
         Perform a single training step.
         """
@@ -95,10 +100,15 @@ class DDIMDiffusionModel(ModelInterface):
         t = torch.randint(0, self.num_timesteps, (batch_size,), device=self.device).long()
 
         noise = torch.randn_like(x_start) if noise is None else noise
-        x_t = self.q_sample(x_start, t, noise)
+        x_t, noise_mult = self.q_sample(x_start, t, noise)
 
         # Predict noise
         eps_pred = self.model(x_t, x_cond, t)
 
-        loss = F.mse_loss(eps_pred, noise)
+        if ms1_loss_weight > 0.0:
+            loss = (1 - ms1_loss_weight) * F.mse_loss(
+                eps_pred, noise * noise_mult
+            ) + ms1_loss_weight * F.mse_loss(torch.sum(x_t - eps_pred, dim=-1), x_cond)
+        else:
+            loss = F.mse_loss(eps_pred, noise * noise_mult)
         return loss

dquartic/model/model_interface.py
@@ -658,7 +658,7 @@ class ModelInterface(object):
     def _train_one_batch(self, x_start, x_cond, x_noise):
         """Train one batch"""
         self.optimizer.zero_grad()
-        loss = self.train_step(x_start, x_cond, x_noise)
+        loss = self.train_step(x_start, x_cond, noise=x_noise)
         loss.backward()
         torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
         self.optimizer.step()

dquartic/model/unet1d.py
@@ -1,4 +1,5 @@
 """ adapted from @lucidrains 's DDPM implementation """
+
 """ https://github.com/lucidrains/denoising-diffusion-pytorch/blob/main/denoising_diffusion_pytorch/denoising_diffusion_pytorch_1d.py """
 import math
 from collections import namedtuple
@@ -17,20 +18,26 @@ from rotary_embedding_torch import RotaryEmbedding
 
 # constants
 
-AttentionConfig = namedtuple('AttentionConfig', ['enable_flash', 'enable_math', 'enable_mem_efficient'])
+AttentionConfig = namedtuple(
+    "AttentionConfig", ["enable_flash", "enable_math", "enable_mem_efficient"]
+)
 
 # helpers functions
 
+
 def exists(x):
     return x is not None
+
 
 def default(val, d):
     if exists(val):
         return val
     return d() if callable(d) else d
 
+
 def once(fn):
     called = False
+
     @wraps(fn)
     def inner(x):
         nonlocal called
@@ -38,11 +45,14 @@ def once(fn):
             return
         called = True
         return fn(x)
+
     return inner
+
 
 print_once = once(print)
 
 # small helper modules
+
 
 class Residual(Module):
     def __init__(self, fn):
@@ -52,14 +62,17 @@ class Residual(Module):
     def forward(self, x, *args, **kwargs):
         return self.fn(x, *args, **kwargs) + x
 
-def Upsample(dim, dim_out = None):
+
+def Upsample(dim, dim_out=None):
     return nn.Sequential(
-        nn.Upsample(scale_factor = 2, mode = 'nearest'),
-        nn.Conv1d(dim, default(dim_out, dim), 3, padding = 1)
+        nn.Upsample(scale_factor=2, mode="nearest"),
+        nn.Conv1d(dim, default(dim_out, dim), 3, padding=1),
     )
 
-def Downsample(dim, dim_out = None):
+
+def Downsample(dim, dim_out=None):
     return nn.Conv1d(dim, default(dim_out, dim), 4, 2, 1)
+
 
 class RMSNorm(Module):
     def __init__(self, dim):
@@ -67,7 +80,8 @@ class RMSNorm(Module):
         self.g = nn.Parameter(torch.ones(1, dim, 1))
 
     def forward(self, x):
-        return F.normalize(x, dim = 1) * self.g * (x.shape[1] ** 0.5)
+        return F.normalize(x, dim=1) * self.g * (x.shape[1] ** 0.5)
+
 
 class PreNorm(Module):
     def __init__(self, dim, fn):
@@ -79,10 +93,12 @@ class PreNorm(Module):
         x = self.norm(x)
         return self.fn(x, *args, **kwargs)
 
+
 # sinusoidal positional embeds
 
+
 class SinusoidalPosEmb(Module):
-    def __init__(self, dim, theta = 10000):
+    def __init__(self, dim, theta=10000):
         super().__init__()
         self.dim = dim
         self.theta = theta
@@ -96,17 +112,19 @@ class SinusoidalPosEmb(Module):
         emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
         return emb
 
+
 # building block modules
 
+
 class Block(Module):
-    def __init__(self, dim, dim_out, dropout = 0.):
+    def __init__(self, dim, dim_out, dropout=0.0):
         super().__init__()
-        self.proj = nn.Conv1d(dim, dim_out, 3, padding = 1)
+        self.proj = nn.Conv1d(dim, dim_out, 3, padding=1)
         self.norm = RMSNorm(dim_out)
         self.act = nn.SiLU()
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x, scale_shift = None):
+    def forward(self, x, scale_shift=None):
         x = self.proj(x)
         x = self.norm(x)
 
@@ -117,48 +135,49 @@ class Block(Module):
         x = self.act(x)
         return self.dropout(x)
 
-class ResnetBlock(Module):
-    def __init__(self, dim, dim_out, *, time_emb_dim = None, dropout = 0.):
-        super().__init__()
-        self.mlp = nn.Sequential(
-            nn.SiLU(),
-            nn.Linear(time_emb_dim, dim_out * 2)
-        ) if exists(time_emb_dim) else None
 
-        self.block1 = Block(dim, dim_out, dropout = dropout)
+class ResnetBlock(Module):
+    def __init__(self, dim, dim_out, *, time_emb_dim=None, dropout=0.0):
+        super().__init__()
+        self.mlp = (
+            nn.Sequential(nn.SiLU(), nn.Linear(time_emb_dim, dim_out * 2))
+            if exists(time_emb_dim)
+            else None
+        )
+
+        self.block1 = Block(dim, dim_out, dropout=dropout)
         self.block2 = Block(dim_out, dim_out)
         self.res_conv = nn.Conv1d(dim, dim_out, 1) if dim != dim_out else nn.Identity()
 
-    def forward(self, x, time_emb = None):
+    def forward(self, x, time_emb=None):
 
         scale_shift = None
         if exists(self.mlp) and exists(time_emb):
             time_emb = self.mlp(time_emb)
-            time_emb = rearrange(time_emb, 'b c -> b c 1')
-            scale_shift = time_emb.chunk(2, dim = 1)
+            time_emb = rearrange(time_emb, "b c -> b c 1")
+            scale_shift = time_emb.chunk(2, dim=1)
 
-        h = self.block1(x, scale_shift = scale_shift)
+        h = self.block1(x, scale_shift=scale_shift)
 
         h = self.block2(h)
 
         return h + self.res_conv(x)
 
+
 # attention
 
+
 class Attend(nn.Module):
-    def __init__(
-        self,
-        dropout = 0.,
-        flash = False,
-        scale = None
-    ):
+    def __init__(self, dropout=0.0, flash=False, scale=None):
         super().__init__()
         self.dropout = dropout
         self.scale = scale
         self.attn_dropout = nn.Dropout(dropout)
 
         self.flash = flash
-        assert not (flash and version.parse(torch.__version__) < version.parse('2.0.0')), 'in order to use flash attention, you must be using pytorch 2.0 or above'
+        assert not (
+            flash and version.parse(torch.__version__) < version.parse("2.0.0")
+        ), "in order to use flash attention, you must be using pytorch 2.0 or above"
 
         # determine efficient attention configs for cuda and cpu
 
@@ -168,15 +187,17 @@ class Attend(nn.Module):
         if not torch.cuda.is_available() or not flash:
             return
 
-        device_properties = torch.cuda.get_device_properties(torch.device('cuda'))
+        device_properties = torch.cuda.get_device_properties(torch.device("cuda"))
 
-        device_version = version.parse(f'{device_properties.major}.{device_properties.minor}')
+        device_version = version.parse(f"{device_properties.major}.{device_properties.minor}")
 
-        if device_version > version.parse('8.0'):
-            print_once('A100 GPU detected, using flash attention if input tensor is on cuda')
+        if device_version > version.parse("8.0"):
+            print_once("A100 GPU detected, using flash attention if input tensor is on cuda")
             self.cuda_config = AttentionConfig(True, False, False)
         else:
-            print_once('Non-A100 GPU detected, using math or mem efficient attention if input tensor is on cuda')
+            print_once(
+                "Non-A100 GPU detected, using math or mem efficient attention if input tensor is on cuda"
+            )
             self.cuda_config = AttentionConfig(False, True, True)
 
     def flash_attn(self, q, k, v):
@@ -196,8 +217,7 @@ class Attend(nn.Module):
 
         with nn.attention.sdpa_kernel(**config._asdict()):
             out = F.scaled_dot_product_attention(
-                q, k, v,
-                dropout_p = self.dropout if self.training else 0.
+                q, k, v, dropout_p=self.dropout if self.training else 0.0
             )
 
         return out
@@ -222,7 +242,7 @@ class Attend(nn.Module):
 
         # attention
 
-        attn = sim.softmax(dim = -1)
+        attn = sim.softmax(dim=-1)
         attn = self.attn_dropout(attn)
 
         # aggregate values
@@ -231,91 +251,90 @@ class Attend(nn.Module):
 
         return out
 
+
 class LinearAttention(Module):
-    def __init__(self, dim, heads = 2, dim_head = 8):
+    def __init__(self, dim, heads=2, dim_head=8):
         super().__init__()
-        self.scale = dim_head ** -0.5
+        self.scale = dim_head**-0.5
         self.heads = heads
         hidden_dim = dim_head * heads
-        self.to_qkv = nn.Conv1d(dim, hidden_dim * 3, 1, bias = False)
+        self.to_qkv = nn.Conv1d(dim, hidden_dim * 3, 1, bias=False)
 
-        self.to_out = nn.Sequential(
-            nn.Conv1d(hidden_dim, dim, 1),
-            RMSNorm(dim)
-        )
+        self.to_out = nn.Sequential(nn.Conv1d(hidden_dim, dim, 1), RMSNorm(dim))
 
     def forward(self, x):
-        qkv = self.to_qkv(x).chunk(3, dim = 1)
-        q, k, v = map(lambda t: rearrange(t, 'b (h c) n -> b h c n', h = self.heads), qkv)
+        qkv = self.to_qkv(x).chunk(3, dim=1)
+        q, k, v = map(lambda t: rearrange(t, "b (h c) n -> b h c n", h=self.heads), qkv)
 
-        q = q.softmax(dim = -2)
-        k = k.softmax(dim = -1)
+        q = q.softmax(dim=-2)
+        k = k.softmax(dim=-1)
 
-        q = q * self.scale        
+        q = q * self.scale
 
-        context = torch.einsum('b h d n, b h e n -> b h d e', k, v)
+        context = torch.einsum("b h d n, b h e n -> b h d e", k, v)
 
-        out = torch.einsum('b h d e, b h d n -> b h e n', context, q)
-        out = rearrange(out, 'b h c n -> b (h c) n', h = self.heads)
+        out = torch.einsum("b h d e, b h d n -> b h e n", context, q)
+        out = rearrange(out, "b h c n -> b (h c) n", h=self.heads)
         return self.to_out(out)
 
+
 class Attention(Module):
-    def __init__(self, dim, heads = 2, dim_head = 8, flash = False, use_xattn = False, cond_dim = 1):
+    def __init__(self, dim, heads=2, dim_head=8, flash=False, use_xattn=False, cond_dim=1):
         super().__init__()
         self.use_xattn = use_xattn
         self.heads = heads
         hidden_dim = dim_head * heads
 
-        self.rotary_emb = RotaryEmbedding(dim = dim_head // 2)
+        self.rotary_emb = RotaryEmbedding(dim=dim_head // 2)
 
-        self.attend = Attend(flash = flash)
+        self.attend = Attend(flash=flash)
 
         if self.use_xattn:
-            self.to_qv = nn.Conv1d(dim, hidden_dim * 2, 1, bias = False)
-            self.to_k = nn.Conv1d(cond_dim, hidden_dim, 1, bias = False)
+            self.to_qv = nn.Conv1d(dim, hidden_dim * 2, 1, bias=False)
+            self.to_k = nn.Conv1d(cond_dim, hidden_dim, 1, bias=False)
         else:
-            self.to_qkv = nn.Conv1d(dim, hidden_dim * 3, 1, bias = False)
+            self.to_qkv = nn.Conv1d(dim, hidden_dim * 3, 1, bias=False)
 
         self.to_out = nn.Conv1d(hidden_dim, dim, 1)
 
-    def forward(self, x, cond = None):
+    def forward(self, x, cond=None):
         if self.use_xattn and exists(cond):
-            qv = self.to_qv(x).chunk(2, dim = 1)
-            q, v = map(lambda t: rearrange(t, 'b (h c) n -> b h n c', h = self.heads), qv)
-
-            if len(cond.shape) == 2:
-                cond = rearrange(cond, 'b n -> b () n')
-
-            k = rearrange(self.to_k(cond), 'b (h c) n -> b h n c', h = self.heads)
+            qv = self.to_qv(x).chunk(2, dim=1)
+            q, v = map(lambda t: rearrange(t, "b (h c) n -> b h n c", h=self.heads), qv)
+            k = rearrange(self.to_k(cond), "b (h c) n -> b h n c", h=self.heads)
         else:
-            qkv = self.to_qkv(x).chunk(3, dim = 1)
-            q, k, v = map(lambda t: rearrange(t, 'b (h c) n -> b h n c', h = self.heads), qkv)
+            qkv = self.to_qkv(x).chunk(3, dim=1)
+            q, k, v = map(lambda t: rearrange(t, "b (h c) n -> b h n c", h=self.heads), qkv)
 
         q = self.rotary_emb.rotate_queries_or_keys(q)
         k = self.rotary_emb.rotate_queries_or_keys(k)
 
         out = self.attend(q, k, v)
 
-        out = rearrange(out, 'b h n d -> b (h d) n')
+        out = rearrange(out, "b h n d -> b (h d) n")
         return self.to_out(out)
 
+
 # model
+
 
 class Unet1D(Module):
     def __init__(
         self,
         dim,
-        init_dim = None,
-        out_dim = None,
+        init_dim=None,
+        out_dim=None,
         dim_mults=(1, 2, 4, 8),
-        channels = 3,
-        dropout = 0.,
-        has_condition = False,
-        learned_variance = False,
-        sinusoidal_pos_emb_theta = 10000,
-        attn_dim_head = 8,
-        attn_heads = 2,
-        attn_downsampled_l = 625
+        channels=3,
+        dropout=0.0,
+        has_condition=False,
+        cond_channels=None,
+        cond_init_dim=None,
+        learned_variance=False,
+        sinusoidal_pos_emb_theta=10000,
+        attn_dim_head=8,
+        attn_heads=2,
+        attn_downsampled_l=625,
     ):
         super().__init__()
 
@@ -326,7 +345,7 @@ class Unet1D(Module):
         input_channels = channels
 
         init_dim = default(init_dim, dim)
-        self.init_conv = nn.Conv1d(input_channels, init_dim, 7, padding = 3)
+        self.init_conv = nn.Conv1d(input_channels, init_dim, 7, padding=3)
 
         dims = [init_dim, *map(lambda m: dim * m, dim_mults)]
         in_out = list(zip(dims[:-1], dims[1:]))
@@ -334,15 +353,21 @@ class Unet1D(Module):
         # time embeddings
 
         time_dim = dim * 4
-        sinu_pos_emb = SinusoidalPosEmb(dim, theta = sinusoidal_pos_emb_theta)
+        sinu_pos_emb = SinusoidalPosEmb(dim, theta=sinusoidal_pos_emb_theta)
         self.time_mlp = nn.Sequential(
-            sinu_pos_emb,
-            nn.Linear(dim, time_dim),
-            nn.GELU(),
-            nn.Linear(time_dim, time_dim)
+            sinu_pos_emb, nn.Linear(dim, time_dim), nn.GELU(), nn.Linear(time_dim, time_dim)
         )
 
-        resnet_block = partial(ResnetBlock, time_emb_dim = time_dim, dropout = dropout)
+        resnet_block = partial(ResnetBlock, time_emb_dim=time_dim, dropout=dropout)
+
+        # conditioning signal
+
+        if self.has_condition:
+            self.cond_proj = nn.Sequential(
+                nn.Conv1d(cond_channels, cond_init_dim, 3, padding=1),
+                nn.GELU(),
+                nn.Conv1d(cond_init_dim, cond_init_dim, 1),
+            )
 
         # layers
 
@@ -353,27 +378,54 @@ class Unet1D(Module):
         for ind, (dim_in, dim_out) in enumerate(in_out):
             is_last = ind >= (num_resolutions - 1)
 
-            self.downs.append(ModuleList([
-                resnet_block(dim_in, dim_in),
-                resnet_block(dim_in, dim_in),
-                Residual(PreNorm(dim_in, LinearAttention(dim_in))),
-                Downsample(dim_in, dim_out) if not is_last else nn.Conv1d(dim_in, dim_out, 3, padding = 1)
-            ]))
+            self.downs.append(
+                ModuleList(
+                    [
+                        resnet_block(dim_in, dim_in),
+                        resnet_block(dim_in, dim_in),
+                        Residual(PreNorm(dim_in, LinearAttention(dim_in))),
+                        (
+                            Downsample(dim_in, dim_out)
+                            if not is_last
+                            else nn.Conv1d(dim_in, dim_out, 3, padding=1)
+                        ),
+                    ]
+                )
+            )
 
         mid_dim = dims[-1]
         self.mid_block1 = resnet_block(mid_dim * attn_downsampled_l, mid_dim * attn_downsampled_l)
-        self.mid_attn = Residual(PreNorm(mid_dim * attn_downsampled_l, Attention(mid_dim * attn_downsampled_l, dim_head = attn_dim_head, heads = attn_heads)))
+        self.mid_attn = Residual(
+            PreNorm(
+                mid_dim * attn_downsampled_l,
+                Attention(
+                    mid_dim * attn_downsampled_l,
+                    dim_head=attn_dim_head,
+                    heads=attn_heads,
+                    use_xattn=self.has_condition,
+                    cond_dim=cond_init_dim,
+                ),
+            )
+        )
         self.mid_block2 = resnet_block(mid_dim * attn_downsampled_l, mid_dim * attn_downsampled_l)
 
         for ind, (dim_in, dim_out) in enumerate(reversed(in_out)):
             is_last = ind == (len(in_out) - 1)
 
-            self.ups.append(ModuleList([
-                resnet_block(dim_out + dim_in, dim_out),
-                resnet_block(dim_out + dim_in, dim_out),
-                Residual(PreNorm(dim_out, LinearAttention(dim_out))),
-                Upsample(dim_out, dim_in) if not is_last else  nn.Conv1d(dim_out, dim_in, 3, padding = 1)
-            ]))
+            self.ups.append(
+                ModuleList(
+                    [
+                        resnet_block(dim_out + dim_in, dim_out),
+                        resnet_block(dim_out + dim_in, dim_out),
+                        Residual(PreNorm(dim_out, LinearAttention(dim_out))),
+                        (
+                            Upsample(dim_out, dim_in)
+                            if not is_last
+                            else nn.Conv1d(dim_out, dim_in, 3, padding=1)
+                        ),
+                    ]
+                )
+            )
 
         default_out_dim = channels * (1 if not learned_variance else 2)
         self.out_dim = default(out_dim, default_out_dim)
@@ -382,10 +434,15 @@ class Unet1D(Module):
         self.final_conv = nn.Conv1d(init_dim, self.out_dim, 1)
 
     def forward(self, x, x_cond, time):
-        x = rearrange(x, 'b c n -> c b n')
+        x = rearrange(x, "b c n -> c b n")
         x = self.init_conv(x)
         r = x.clone()
         t = self.time_mlp(time)
+        c = (
+            self.cond_proj(rearrange(x_cond, "b n -> b () n") if len(x_cond.shape) == 2 else x_cond)
+            if self.has_condition
+            else None
+        )
 
         h = []
 
@@ -400,25 +457,25 @@ class Unet1D(Module):
             x = downsample(x)
 
         c, _, n = x.shape
-        x = rearrange(x, 'c b n -> 1 (b n) c', c = c, n = n)
+        x = rearrange(x, "c b n -> 1 (b n) c", c=c, n=n)
         x = self.mid_block1(x, t[0].unsqueeze(0))
-        x = self.mid_attn(x, cond = x_cond if self.has_condition else None)
+        x = self.mid_attn(x, cond=c)
         x = self.mid_block2(x, t[0].unsqueeze(0))
-        x = rearrange(x, '1 (b n) c -> c b n', c = c, n = n)
+        x = rearrange(x, "1 (b n) c -> c b n", c=c, n=n)
 
         for block1, block2, attn, upsample in self.ups:
-            x = torch.cat((x, h.pop()), dim = 1)
+            x = torch.cat((x, h.pop()), dim=1)
             x = block1(x, t)
 
-            x = torch.cat((x, h.pop()), dim = 1)
+            x = torch.cat((x, h.pop()), dim=1)
             x = block2(x, t)
             x = attn(x)
 
             x = upsample(x)
 
-        x = torch.cat((x, r), dim = 1)
+        x = torch.cat((x, r), dim=1)
 
         x = self.final_res_block(x, t)
         x = self.final_conv(x)
-        x = rearrange(x, 'c b n -> b c n')
+        x = rearrange(x, "c b n -> b c n")
         return x