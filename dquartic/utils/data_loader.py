66c88f38b0871d59d5563ee61f2ae9cf547449b6
LLYX <leon.xu@mail.utoronto.ca>
2024-10-24 04:23:47 -0400

    Merge changes from pull

--- dquartic/utils/data_loader.py
+++ dquartic/utils/data_loader.py
@@ -9,28 +9,29 @@ from typing import Literal
 class DIAMSDataset(Dataset):
     """
     A dataset class for loading DIA-MS data.
-    
+
     Args:
         ms2_file (str): The file path to the MS2 data.
         ms1_file (str): The file path to the MS1 data.
         normalize (Literal[None, 'minmax'], optional): The normalization method. Defaults to None.
-        
+
     Attributes:
         ms2_data (numpy.ndarray): The loaded MS2 data.
         ms1_data (numpy.ndarray): The loaded MS1 data.
         normalize (Literal[None, 'minmax']): The normalization method.
         used_pairs (set): A set to keep track of used pairs.
         epoch_reset (bool): A flag to indicate if the epoch has been reset.
-        
+
     Methods:
         __len__(): Returns the length of the dataset.
         reset_epoch(): Resets the used pairs at the start of each epoch.
         __getitem__(idx): Retrieves an item from the dataset.
     """
-    def __init__(self, ms2_file, ms1_file, normalize: Literal[None, 'minmax'] = None):
-        self.ms2_data = np.load(ms2_file, mmap_mode='r')
-        self.ms1_data = np.load(ms1_file, mmap_mode='r')
-        
+
+    def __init__(self, ms2_file, ms1_file, normalize: Literal[None, "minmax"] = None):
+        self.ms2_data = np.load(ms2_file, mmap_mode="r")
+        self.ms1_data = np.load(ms1_file, mmap_mode="r")
+
         self.normalize = normalize
         self.used_pairs = set()
         self.epoch_reset = False
@@ -70,21 +71,21 @@ class DIAMSDataset(Dataset):
         ms1_sample_split_1 = self.ms1_data[idx_1]
         ms2_sample_split_2 = self.ms2_data[idx_2]
         ms1_sample_split_2 = self.ms1_data[idx_2]
-        
-        if self.normalize == 'minmax':
+
+        if self.normalize == "minmax":
             self.ms2_min = np.min([ms2_sample_split_1.min(), ms2_sample_split_2.min()])
             self.ms2_max = np.max([ms2_sample_split_1.max(), ms2_sample_split_2.max()])
             self.ms1_min = np.min([ms1_sample_split_1.min()])
             self.ms1_max = np.max([ms1_sample_split_1.max()])
-            
+
             ms2_sample_split_1 = (ms2_sample_split_1 - self.ms2_min) / (self.ms2_max - self.ms2_min)
             ms1_sample_split_1 = (ms1_sample_split_1 - self.ms1_min) / (self.ms1_max - self.ms1_min)
             ms2_sample_split_2 = (ms2_sample_split_2 - self.ms2_min) / (self.ms2_max - self.ms2_min)
             ms1_sample_split_2 = (ms1_sample_split_2 - self.ms1_min) / (self.ms1_max - self.ms1_min)
 
-        return (torch.from_numpy(ms2_sample_split_1.astype(np.float32)),
-                torch.from_numpy(ms1_sample_split_1.astype(np.float32)),
-                torch.from_numpy(ms2_sample_split_2.astype(np.float32)),
-                torch.from_numpy(ms1_sample_split_2.astype(np.float32)))
-
-
+        return (
+            torch.from_numpy(ms2_sample_split_1.astype(np.float32)),
+            torch.from_numpy(ms1_sample_split_1.astype(np.float32)),
+            torch.from_numpy(ms2_sample_split_2.astype(np.float32)),
+            torch.from_numpy(ms1_sample_split_2.astype(np.float32)),
+        )
