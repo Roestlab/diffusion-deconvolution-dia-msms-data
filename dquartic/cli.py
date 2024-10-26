c78fa8f8308b3c69334693d9433e7ea39c0054b7
singjc <justincsing@gmail.com>
2024-10-26 12:34:17 -0400

    refactor: CLI args to json config

--- dquartic/cli.py
+++ dquartic/cli.py
@@ -1,5 +1,7 @@
 import click
+import ast
 from .utils.data_loader import DIAMSDataset
+from .utils.config_loader import load_train_config, generate_train_config
 from .model.building_blocks import CustomTransformer
 from .model.model import DDIMDiffusionModel
 from .model.unet1d import Unet1D
@@ -8,94 +10,49 @@ import torch
 import wandb
 
 
-@click.group()
+# https://stackoverflow.com/a/47730333
+class PythonLiteralOption(click.Option):
+    def type_cast_value(self, ctx, value):
+        if not isinstance(value, str):  # required for Click>=8.0.0
+            return value
+        try:
+            return ast.literal_eval(value)
+        except Exception:
+            raise click.BadParameter(value)
+
+
+@click.group(chain=True)
+@click.version_option()
 def cli():
-    pass
+    """
+    Diffusion Deconvolution of DIA-MS/MS Data (D^4)
+    """
 
 
 @cli.command()
-@click.option("--epochs", default=1000, help="Number of epochs to train")
-@click.option(
-    "--warmup-epochs", default=5, help="Number of warmup epochs for learning rate scheduler"
-)
-@click.option("--batch-size", default=1, help="Batch size for training")
-@click.option("--learning-rate", default=1e-6, help="Learning rate for optimizer")
-@click.option("--hidden-dim", default=1024, help="Hidden dimension for the model")
-@click.option("--num-heads", default=8, help="Number of attention heads")
-@click.option("--num-layers", default=8, help="Number of transformer layers")
-@click.option("--num-timesteps", default=1000, help="Number of timesteps for diffusion model")
-@click.option("--beta-start", default=0.001, help="Start value for beta scheduler")
-@click.option("--beta-end", default=0.00125, help="End value for beta scheduler")
-@click.option("--pred-type", default="eps", help="Prediction type, 'eps' for predicting noise,'x0' for predicting target.")
-@click.option("--ms1-loss-weight", default=0.0, help="Weight for MS1 loss")
-@click.option(
-    "--use-model",
-    default="CustomTransformer",
-    help="Model class to use. (CustomTransformer, Unet1D)",
-)
-# Data settings
-@click.option("--normalize", default=None, help="Normalization method. (None, minmax)")
-@click.option("--ms2-data-path", default="bigdata/ms2_data_cat_int32.npy", help="Path to MS2 data")
-@click.option("--ms1-data-path", default="bigdata/ms1_data_int32.npy", help="Path to MS1 data")
-@click.option("--checkpoint-path", default="best_model.ckpt", help="Path to save the best model")
-@click.option("--use-wandb", is_flag=True, help="Enable Weights & Biases logging")
-@click.option("--threads", default=4, help="Number of threads for data loading")
-# Wandb settings
-@click.option("--wandb-project", default="dquartic", help="Weigths & Biases project name")
-@click.option(
-    "--wandb-name",
-    default=None,
-    help="Weigths & Biases name. If None, Wandb will generate a random name",
-)
-@click.option("--wandb-id", default=None, help="Weigths & Biases run ID")
-@click.option(
-    "--wandb-resume",
-    default=None,
-    help="Weigths & Biases resume ID if run crashed or stopped early. Allowed values are None, allow, must, never",
-)
-@click.option(
-    "--wandb-architecture",
-    default="DDIM(CustomTransformer)",
-    help="Weigths & Biases model architecture name",
-)
-@click.option("--wandb-dataset", default="Josh_GPF_DIA", help="Weigths & Biases dataset name")
-@click.option(
-    "--wandb-mode",
-    default="offline",
-    help="Weigths & Biases mode. (offline, online). Default is offline, which means metrics are only saved locally. You will need to run wandb sync to upload the metrics to the cloud.",
-)
+@click.argument("config-path", type=click.Path(exists=True), required=True)
+@click.option("--ms2-data-path", default=None, help="Path to MS2 data, overides config file")
+@click.option("--ms1-data-path", default=None, help="Path to MS1 data, overides config file")
+@click.option("--batch-size", default=None, help="Batch size for training, overides config file")
+@click.option("--checkpoint-path", default=None, help="Path to save the best model, overides config file")
+@click.option("--use-wandb", default=None, cls=PythonLiteralOption, help="Use wandb for logging, overides config file")
+@click.option("--threads", default=None, help="Number of threads for data loading, overides config file")
 def train(
-    epochs,
-    warmup_epochs,
-    batch_size,
-    learning_rate,
-    hidden_dim,
-    num_heads,
-    num_layers,
-    num_timesteps,
-    beta_start,
-    beta_end,
-    pred_type,
-    ms1_loss_weight,
-    use_model,
-    normalize,
+    config_path,
     ms2_data_path,
     ms1_data_path,
+    batch_size,
     checkpoint_path,
     use_wandb,
-    threads,
-    wandb_project,
-    wandb_name,
-    wandb_id,
-    wandb_resume,
-    wandb_architecture,
-    wandb_dataset,
-    wandb_mode,
+    threads
 ):
     """
     Train a DDIM model on the DIAMS dataset.
     """
     if torch.cuda.is_available():
+        click.echo("--" * 30)
+        click.echo("GPU Information:")
+        click.echo("--" * 30)
         for i in range(torch.cuda.device_count()):
             print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
             print(
@@ -103,73 +60,86 @@ def train(
             )
             print(f"Allocated Memory: {torch.cuda.memory_allocated(i) / (1024 ** 2)} MB")
             print(f"Cached Memory: {torch.cuda.memory_reserved(i) / (1024 ** 2)} MB")
+        click.echo("--" * 30)
     else:
+        click.echo("--" * 30)
         print("No GPUs available.")
+        click.echo("--" * 30)
+    
+    click.echo(f"Info: Loading config from {config_path}")
+        
+    config = load_train_config(config_path, ms2_data_path=ms2_data_path, ms1_data_path=ms1_data_path, batch_size=batch_size, checkpoint_path=checkpoint_path, use_wandb=use_wandb, threads=threads)
+    
+    ms2_data_path = config['data']['ms2_data_path']
+    ms1_data_path = config['data']['ms1_data_path']
+    batch_size = config['model']['batch_size']
+    checkpoint_path = config['model']['checkpoint_path']
+    use_wandb = config['wandb']['use_wandb']
+    threads = config['threads']
 
-    dataset = DIAMSDataset(ms2_data_path, ms1_data_path, normalize=normalize)
+    dataset = DIAMSDataset(ms2_data_path, ms1_data_path, normalize=config['data']['normalize'])
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=threads)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    if use_model == "Unet1D":
+    if config['model']['use_model'] == "Unet1D":
         model_init = {
-            "dim": 4,
-            "channels": 1,
-            "dim_mults": (1, 2, 2, 3, 3, 4, 4),
-            "cond_channels": 1,
-            "cond_init_dim": 4,
-            "has_condition": True,
+            "dim": config['model']['Unet1D']['dim'],
+            "channels": config['model']['Unet1D']['channels'],
+            "dim_mults": tuple(config['model']['Unet1D']['dim_mults']),
+            "cond_channels": config['model']['Unet1D']['cond_channels'],
+            "cond_init_dim": config['model']['Unet1D']['cond_init_dim'],
+            "has_condition": config['model']['Unet1D']['has_condition'],
         }
         model = Unet1D(**model_init).to(device)
-    elif use_model == "CustomTransformer":
+    elif config['model']['use_model'] == "CustomTransformer":
         model_init = {
-            "input_dim": 40000,
-            "hidden_dim": hidden_dim,
-            "num_heads": num_heads,
-            "num_layers": num_layers,
+            "input_dim": config['model']['CustomTransformer']['input_dim'],
+            "hidden_dim": config['model']['CustomTransformer']['hidden_dim'],
+            "num_heads": config['model']['CustomTransformer']['num_heads'],
+            "num_layers": config['model']['CustomTransformer']['num_layers'],
         }
         model = CustomTransformer(**model_init).to(device)
     else:
-        raise ValueError(f"Invalid model class: {use_model}")
+        raise ValueError(f"Invalid model class: {config['model']['use_model']}")
     diffusion_model = DDIMDiffusionModel(
         model_class=model,
-        num_timesteps=num_timesteps,
-        beta_start=beta_start,
-        beta_end=beta_end,
-        pred_type=pred_type,
-        ms1_loss_weight=ms1_loss_weight,
+        num_timesteps=config['model']['num_timesteps'],
+        beta_start=config['model']['beta_start'],
+        beta_end=config['model']['beta_end'],
+        pred_type=config['model']['pred_type'],
+        ms1_loss_weight=config['model']['ms1_loss_weight'],
         device=device,
     )
 
     if use_wandb:
         wandb.init(
-            project=wandb_project,
-            name=wandb_name,
-            id=wandb_id,
-            resume=wandb_resume,
+            project=config['wandb']['wandb_project'],
+            name=config['wandb']['wandb_name'],
+            id=config['wandb']['wandb_id'],
+            resume=config['wandb']['wandb_resume'],
             config={
-                "learning_rate": learning_rate,
-                "architecture": wandb_architecture,
-                "dataset": wandb_dataset,
-                "epochs": epochs,
-                "batch_size": batch_size,
-                "beta_start": beta_start,
-                "beta_end": beta_end,
-                "ms1_loss_weight": ms1_loss_weight,
-                "model": use_model,
-                "model_params": vars(model),
-                **model_init,
+                "architecture": config['wandb']['wandb_architecture'],
+                "dataset": config['wandb']['wandb_dataset'],
+                **config['model']
             },
             settings=wandb.Settings(start_method="fork"),
-            mode=wandb_mode,
+            mode=config['wandb']['wandb_mode'],
         )
 
     diffusion_model.train(
-        data_loader, batch_size, epochs, warmup_epochs, learning_rate, use_wandb, checkpoint_path
+        data_loader, batch_size, config['model']['num_epochs'], config['model']['warmup_epochs'], config['model']['learning_rate'], use_wandb, checkpoint_path
     )
 
     if use_wandb:
         wandb.finish()
 
+@cli.command()
+@click.argument("config-path", type=click.Path(exists=True), required=True)
+def generate_config(config_path):
+    """
+    Generate a training configuration file.
+    """
+    click.echo(f"Info: Generating config at {config_path}")
+    generate_train_config(config_path)
 
-if __name__ == "__main__":
-    cli()
+
---
+++ dquartic/utils/config_loader.py
@@ -0,0 +1,95 @@
+import json
+
+
+def load_train_config(config_path: str, **kwargs):
+    """
+    Load the training configuration from a JSON file and override the parameters with the provided keyword arguments if passed.
+
+    Parameters:
+        config_path (str): The path to the JSON configuration file.
+        **kwargs: Keyword arguments to override the configuration parameters.
+
+    Returns:
+        dict: The loaded configuration parameters with overridden values.
+    """
+    with open(config_path, "r") as f:
+        config_params = json.load(f)
+
+    # Override the config params with the keyword arguments
+    if "ms2_data_path" in kwargs:
+        config_params["data"]["ms2_data_path"] = kwargs["ms2_data_path"]
+
+    if "ms1_data_path" in kwargs:
+        config_params["data"]["ms1_data_path"] = kwargs["ms1_data_path"]
+
+    if "batch_size" in kwargs:
+        config_params["model"]["batch_size"] = kwargs["batch_size"]
+
+    if "checkpoint_path" in kwargs:
+        config_params["model"]["checkpoint_path"] = kwargs["checkpoint_path"]
+
+    if "use_wandb" in kwargs:
+        config_params["wandb"]["use_wandb"] = kwargs["use_wandb"]
+
+    if "threads" in kwargs:
+        config_params["threads"] = kwargs["threads"]
+
+    return config_params
+
+
+def generate_train_config(config_path: str):
+    """
+    Generate a training configuration file.
+
+    Parameters:
+        config_path (str): The path to save the generated JSON configuration file.
+    """
+    full_config = {
+        "data": {
+            "ms2_data_path": "data/MS2_data.csv",
+            "ms1_data_path": "data/MS1_data.csv",
+            "normalize": "minmax",
+        },
+        "model": {
+            "checkpoint_path": "best_model.ckpt",
+            "num_epochs": 10000,
+            "warmup_epochs": 5,
+            "batch_size": 1,
+            "learning_rate": 0.00001,
+            "num_timesteps": 1000,
+            "beta_start": 0.001,
+            "beta_end": 0.00125,
+            "pred_type": "eps",
+            "ms1_loss_weight": 0.0,
+            "use_model": "Unet1D",
+            "CustomTransformer": {
+                "input_dim": 40000,
+                "hidden_dim": 1024,
+                "num_heads": 8,
+                "num_layers": 8,
+            },
+            "Unet1D": {
+                "dim": 4,
+                "channels": 1,
+                "dim_mults": [1, 2, 2, 3, 3, 4, 4],
+                "cond_channels": 1,
+                "cond_init_dim": 4,
+                "has_condition": True,
+            },
+        },
+        "wandb": {
+            "use_wandb": True,
+            "wandb_project": "dquartic",
+            "wandb_name": None,
+            "wandb_id": None,
+            "wandb_resume": None,
+            "wandb_architecture": "DDIM(Unet1D)",
+            "wandb_dataset": "MS2",
+            "wandb_mode": "offline",
+        },
+        "threads": 4,
+    }
+
+    # Save the configuration to a JSON file
+    with open(config_path, "w") as f:
+        json.dump(full_config, f, indent=4)
